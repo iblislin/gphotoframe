02e4d34dfbad18f1880d5a772907723c0bebc2ee
yendo0206 <yendo0206@localhost>
2009-05-16 08:57:17 +0000

    Fixed bugs for the non-install mode.

--- lib/utils/autostart.py
+++ lib/utils/autostart.py
@@ -15,20 +15,26 @@ class AutoStart(object):
             '/.config/autostart/%s.desktop' % app_name
         self.key = 'X-GNOME-Autostart-enabled'
 
-        load_file = self.local_entry \
-            if os.access(self.local_entry, os.R_OK) else system_entry
+        self.load_file = self.local_entry \
+            if os.access(self.local_entry, os.R_OK) else system_entry \
+            if os.access(system_entry, os.R_OK) else None
         self.entry = DesktopEntry()
-        self.entry.parse(load_file)
+        if self.load_file:
+            self.entry.parse(self.load_file)
+
+    def check_enable(self):
+        state = True if self.load_file else False
+        return state
 
     def get(self):
         state = True if self.entry.get(self.key) == 'true' else False
         return state
 
     def set(self, state):
-        state_str = 'true' if state else 'false'
-        self.entry.set(self.key, state_str, 'Desktop Entry')
-        self.entry.write(self.local_entry)
-        print self.local_entry
+        if self.check_enable:
+            state_str = 'true' if state else 'false'
+            self.entry.set(self.key, state_str, 'Desktop Entry')
+            self.entry.write(self.local_entry)
 
 if __name__ == "__main__":
     auto_start = AutoStart('gphotoframe')
