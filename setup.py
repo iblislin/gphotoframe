0bdd16e4a205008c9f581400fc2b7b82f7c49d8f
yendo0206 <yendo0206@localhost>
2009-05-16 12:15:23 +0000

    Miscellaneous modifications.

--- setup.py
+++ setup.py
@@ -2,19 +2,21 @@
 
 from distutils.core import setup
 from DistUtilsExtra.command import *
+import lib.constants as constants
 
-setup(name='gphotoframe',
-      version='0.1.1',
-      description='Gnome Photo Frame',
-      author='Yoshizumi Endo',
-      author_email='y-endo@ceres.dti.ne.jp',
-      url='http://code.google.com/p/gphotoframe/',
+setup(name = 'gphotoframe',
+      version = constants.VERSION,
+      description = 'Gnome Photo Frame',
+      author = 'Yoshizumi Endo',
+      author_email = 'y-endo@ceres.dti.ne.jp',
+      url = 'http://code.google.com/p/gphotoframe/',
+      license = 'GPL3',
       package_dir = {'gphotoframe' : 'lib'},
-      packages=['gphotoframe', 'gphotoframe.plugins', 'gphotoframe.utils'],
-      scripts=['gphotoframe'],
-      data_files=[('share/gphotoframe', ['gphotoframe.glade'])],
-      cmdclass = { "build" : build_extra.build_extra,
-                   "build_i18n" : build_i18n.build_i18n,
-                   "build_help" : build_help.build_help,
-                   "build_icons" : build_icons.build_icons }
+      packages = ['gphotoframe', 'gphotoframe.plugins', 'gphotoframe.utils'],
+      scripts = ['gphotoframe'],
+      data_files = [('share/gphotoframe', ['gphotoframe.glade'])],
+      cmdclass = {"build" : build_extra.build_extra,
+                  "build_i18n" : build_i18n.build_i18n,
+                  "build_help" : build_help.build_help,
+                  "build_icons" : build_icons.build_icons}
       )
