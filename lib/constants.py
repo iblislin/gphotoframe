4bc870972b66e7f9cc7a6e3a7e07f0767a04cee6
yendo0206 <yendo0206@localhost>
2010-01-25 15:44:35 +0900

    Added support for images in <content:encoded>, feed titles in Photo Source Preferences.

--- lib/constants.py
+++ lib/constants.py
@@ -1,7 +1,7 @@
 import os
 from os.path import join, abspath,  dirname
 
-VERSION = '0.5'
+VERSION = '0.6-b1'
 APP_NAME = 'gphotoframe'
 
 SHARED_DATA_DIR = abspath(join(dirname(__file__), '../share'))
