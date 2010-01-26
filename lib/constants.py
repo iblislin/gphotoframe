8b1030e3cd93bdc812aabeef632f6b44b2384e21
yendo0206 <yendo0206@localhost>
2010-01-26 14:43:39 +0900

    Fixed a bug. "Interestingness" API doesn't need any argument. (issue #37)

---
+++ lib/constants.py
@@ -0,0 +1,16 @@
+import os
+from os.path import join, abspath,  dirname
+
+VERSION = '0.6-b1'
+APP_NAME = 'gphotoframe'
+
+SHARED_DATA_DIR = abspath(join(dirname(__file__), '../share'))
+if not os.access(SHARED_DATA_DIR + '/gphotoframe.glade', os.R_OK):
+    SHARED_DATA_DIR = '/usr/share/gphotoframe'
+
+GLADE_FILE = SHARED_DATA_DIR + '/gphotoframe.glade'
+
+user = os.environ.get('USER', os.path.split(os.environ.get('HOME'))[1])
+CACHE_DIR = '/tmp/gphotoframe-' + user + '/'
+if not os.access(CACHE_DIR, os.W_OK):
+    os.makedirs(CACHE_DIR)
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import os
+from distutils.core import setup
+from DistUtilsExtra.command import *
+import lib.constants as constants
+import lib.extras.build_i18n
+
+os.chmod("share/gphotoframe.glade", 0644)
+
+setup(name = 'gphotoframe',
+      version = constants.VERSION,
+      description = 'Gnome Photo Frame',
+      long_description = 'A Photo Frame Gadget for the GNOME Desktop.',
+      author = 'Yoshizumi Endo',
+      author_email = 'y-endo@ceres.dti.ne.jp',
+      url = 'http://code.google.com/p/gphotoframe/',
+      license = 'GPL3',
+      package_dir = {'gphotoframe' : 'lib'},
+      packages = ['gphotoframe', 'gphotoframe.plugins', 'gphotoframe.utils'],
+      scripts = ['gphotoframe'],
+      data_files = [('share/gphotoframe', ['share/gphotoframe.glade']),
+                    ('share/gphotoframe', ['share/rss-16.png']),
+                    ('lib/gnome-screensaver/gnome-screensaver', 
+                     ['gphotoframe-screensaver'])],
+      cmdclass = {"build" : build_extra.build_extra,
+                  "build_i18n" : build_i18n.build_i18n,
+                  "build_help" : build_help.build_help,
+                  "build_icons" : build_icons.build_icons}
+      )
