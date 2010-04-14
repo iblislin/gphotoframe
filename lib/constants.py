7c465e48e48f70e565a241bd0ee0a4065c5f1923
yendo0206 <yendo0206@localhost>
2010-04-14 18:28:41 +0900

    Code refactoring.

--- lib/constants.py
+++ lib/constants.py
@@ -2,7 +2,7 @@ import os
 import getpass
 from os.path import join, abspath, dirname
 
-VERSION = '0.7-b8'
+VERSION = '0.7-b9'
 APP_NAME = 'gphotoframe'
 
 SHARED_DATA_DIR = abspath(join(dirname(__file__), '../share'))
