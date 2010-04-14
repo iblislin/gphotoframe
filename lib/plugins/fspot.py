fdb0fe49b11dbe907c3482444808ffb918ea0af5
yendo0206 <yendo0206@localhost>
2010-04-14 18:28:41 +0900

    Code refactoring.

--- lib/plugins/fspot.py
+++ lib/plugins/fspot.py
@@ -318,30 +318,29 @@ class RateList(list):
     def __init__(self, sql, options, photolist):
         super(RateList, self).__init__()
 
-        self.photolist = photolist
-
         self.db  = FSpotDB()
         self.sql = sql
+        self.photolist = photolist
 
         if not self.db.is_accessible:
             self.total = 0
             return
 
-        self.rate_min = rate_min = options.get('rate_min', 0)
-        self.rate_max = rate_max = options.get('rate_max', 5)
+        self.rate_min = options.get('rate_min', 0)
+        self.rate_max = options.get('rate_max', 5)
         weight   = options.get('rate_weight', 2)
 
-        sql = self.sql.get_statement('rating, COUNT(*)') + ' GROUP BY rating'
+        sql = self.sql.get_statement(
+            'rating, COUNT(*)', None, 
+            self.rate_min, self.rate_max) + ' GROUP BY rating'
         count_list = self.db.fetchall(sql)
-        self.total = sum(x[1] for x in count_list if rate_min <= x[0] <= rate_max)
+        self.total = sum(x[1] for x in count_list)
 
         # initialize all rate couter as 0
         count_dic = dict([(x, 0) for x in xrange(6)])
         count_dic.update(dict(count_list))
 
         for rate, total_in_this in count_dic.items():
-            if not rate_min <= rate <= rate_max:
-                total_in_this = 0
             rate_info = Rate(rate, total_in_this, self.total, weight)
             self.append(rate_info)
 
