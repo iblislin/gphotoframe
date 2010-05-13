5bd588083d1ebb1fe901b5f98795f6c8e06107c5
yendo0206 <yendo0206@localhost>
2010-05-13 16:17:58 +0900

    pacparser -> libproxy.

--- lib/utils/urlgetautoproxy.py
+++ lib/utils/urlgetautoproxy.py
@@ -1,12 +1,30 @@
-import os
+try:
+    import libproxy
+except ImportError:
+    libproxy = False
 
-from ..constants import CACHE_DIR
 from urlget import UrlGetWithProxy
-from proxypac import ParseProxyPac
+
+class AutoProxy(object):
+
+    def get_proxy(self, url):
+        if not libproxy: return ""
+
+        pf = libproxy.ProxyFactory()
+        for proxy in pf.getProxies(url):
+            if proxy.startswith('http'):
+                return proxy
+
+        return ""
 
 class UrlGetWithAutoProxy(UrlGetWithProxy):
 
     def __init__(self, url):
-        cache_file = os.path.join(CACHE_DIR, 'proxy.pac')
-        proxy_url = ParseProxyPac(cache_file).get_proxy(url)
+        proxy_url = AutoProxy().get_proxy(url)
+        print proxy_url
         super(UrlGetWithAutoProxy, self).__init__(proxy_url)
+
+if __name__ == "__main__":
+    pac = ParseProxyPac()
+    print pac.get_proxy("http://master/")
+    print pac.get_proxy("http://master.edu/")
