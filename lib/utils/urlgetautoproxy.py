c4a2e70f976390ae9a0f5404771a27d99c064b8c
yendo0206 <yendo0206@localhost>
2010-05-13 12:24:44 +0900

    Added cache directory support for proxy.pac file.

--- lib/utils/urlgetautoproxy.py
+++ lib/utils/urlgetautoproxy.py
@@ -1,8 +1,12 @@
+import os
+
+from ..constants import CACHE_DIR
 from urlget import UrlGetWithProxy
 from proxypac import ParseProxyPac
 
 class UrlGetWithAutoProxy(UrlGetWithProxy):
 
     def __init__(self, url):
-        proxy = ParseProxyPac().get_proxy(url)
-        super(UrlGetWithAutoProxy, self).__init__(proxy)
+        cache_file = os.path.join(CACHE_DIR, 'proxy.pac')
+        proxy_url = ParseProxyPac(cache_file).get_proxy(url)
+        super(UrlGetWithAutoProxy, self).__init__(proxy_url)
