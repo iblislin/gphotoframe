367ffc8c15540c6e092ad95cf3676ae91e7b900e
yendo0206 <yendo0206@localhost>
2010-05-27 03:37:09 +0900

    Added checking entries without title.

--- lib/plugins/tumblr.py
+++ lib/plugins/tumblr.py
@@ -81,7 +81,8 @@ class TumblrPhotoList(PhotoList):
             if self.target != 'User':
                 owner = post.attrib['tumblelog']
 
-            entry_title = re_nl.sub('\n', photo.get('photo-caption'))
+            caption = photo.get('photo-caption')
+            entry_title = re_nl.sub('\n', caption) if caption else None
 
             data = {'url'        : photo['photo-url-500'],
                     'id'         : post.attrib['id'],
