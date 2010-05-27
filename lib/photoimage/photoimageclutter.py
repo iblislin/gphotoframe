974a51d0ab17bf70f2bf20c3ee8fef5c090a9ef8
yendo0206 <yendo0206@localhost>
2010-05-27 19:33:05 +0900

    Added new gconf key 'plugins/rss/standard_deviation'.

---
+++ lib/photoimage/photoimageclutter.py
@@ -0,0 +1,147 @@
+from __future__ import division
+
+try:
+    import cluttergtk
+    import clutter
+except ImportError:
+    from ..utils.nullobject import Null
+    cluttergtk = Null()
+
+from ..utils.config import GConf
+from actors import *
+from photoimagegtk import *
+
+class PhotoImageClutter(PhotoImage):
+
+    def __init__(self, photoframe):
+        super(PhotoImageClutter, self).__init__(photoframe)
+
+        self.image = self.embed = cluttergtk.Embed()
+        self.stage = self.embed.get_stage()
+        color = self._get_border_color()
+        self.stage.set_color(clutter.color_from_string(color))
+        self.embed.show()
+
+        self.photo_image = Texture(self.stage)
+        self.photo_image.show()
+        self.actors = [ ActorSourceIcon(self.stage),
+                        ActorGeoIcon(self.stage),
+                        ActorFavIcon(self.stage), ]
+
+    def _get_border_color(self):
+        return self.conf.get_string('border_color') or '#edeceb'
+
+    def _set_photo_image(self, pixbuf):
+        self.border = border = self.conf.get_int('border_width', 5)
+
+        self.window_border = 0
+        self.w = pixbuf.get_width()
+        self.h = pixbuf.get_height()
+
+        x, y = self._get_image_position()
+        self._change_texture(pixbuf, x, y)
+
+    def _change_texture(self, pixbuf, x, y):
+        self.photo_image.change(pixbuf, x, y)
+
+        for actor in self.actors:
+            actor.set_icon(self, x, y)
+
+    def _get_image_position(self):
+        return self.border, self.border
+
+    def clear(self):
+        pass
+
+    def on_enter_cb(self, w, e):
+        for actor in self.actors:
+            actor.show(True)
+
+    def on_leave_cb(self, w, e):
+        for actor in self.actors:
+            actor.hide()
+
+    def check_actor(self, stage, event):
+        x, y = int(event.x), int(event.y)
+        actor = self.stage.get_actor_at_pos(clutter.PICK_ALL, x, y)
+        result = (actor != self.photo_image)
+        return result
+
+    def check_mouse_on_window(self):
+        window, x, y = gtk.gdk.window_at_pointer() or [None, None, None]
+        result = window is self.embed.window
+        return result
+
+class PhotoImageClutterFullScreen(PhotoImageClutter, PhotoImageFullScreen):
+
+    def __init__(self, photoframe):
+        super(PhotoImageClutterFullScreen, self).__init__(photoframe)
+
+        self.photo_image2 = Texture(self.stage)
+        self.photo_image2.show()
+        self.actors2 = [ ActorSourceIcon(self.stage),
+                        ActorGeoIcon(self.stage),
+                        ActorFavIcon(self.stage), ]
+        self.first = True # image1 or image2
+
+        self.animation = self.conf.get_bool('ui/animate_fullscreen', False)
+        if self.animation:
+            self.photo_image.set_opacity(0)
+
+    def _change_texture(self, pixbuf, x, y):
+        if not self.animation:
+            super(PhotoImageClutterFullScreen, self)._change_texture(pixbuf, x, y)
+            return
+
+        image1, image2 = self.photo_image, self.photo_image2
+        actors1, actors2 = self.actors, self.actors2
+
+        if not self.first:
+            image1, image2 = image2, image1
+            actors1, actors2 = actors2, actors1
+
+        image1.change(pixbuf, x, y)
+        image1.timeline.fade_in()
+        image2.timeline.fade_out()
+
+        for a1, a2 in zip(actors1, actors2):
+            a1.set_icon(self, x, y)
+            a2.hide(True)
+
+        self.first = not self.first
+
+    def _get_image_position(self):
+        root_w, root_h = self._get_max_display_size()
+        x = (root_w - self.w) / 2
+        y = (root_h - self.h) / 2
+        self.border = 0
+        return x, y
+
+    def _get_border_color(self):
+        return 'black'
+
+    def check_mouse_on_window(self):
+        state = super(PhotoImageClutterFullScreen, self).check_mouse_on_window()
+        result = state if self.photoframe.ui.is_show else False
+        return result
+
+    def on_enter_cb(self, w, e):
+        act = self.actors2 if self.first and self.animation else self.actors
+        for actor in act:
+            actor.show(True)
+
+    def on_leave_cb(self, w, e):
+        act = self.actors2 if self.first and self.animation else self.actors
+        for actor in act:
+            actor.hide()
+
+class PhotoImageClutterScreenSaver(PhotoImageClutterFullScreen,
+                                   PhotoImageScreenSaver):
+
+    def __init__(self, photoframe):
+        super(PhotoImageClutterScreenSaver, self).__init__(photoframe)
+        if not self.conf.get_bool('ui/icons_on_screensaver', False):
+            self.actors = []
+
+    def check_mouse_on_window(self):
+        return False
---
+++ lib/utils/iconimage.py
@@ -0,0 +1,70 @@
+import os
+
+import gtk
+from xdg.BaseDirectory import xdg_cache_home
+
+from .. import constants
+from ..utils.urlgetautoproxy import UrlGetWithAutoProxy
+
+class IconImage(object):
+
+    def __init__(self, icon_name='image-x-generic'):
+        self.icon_name = icon_name
+
+    def get_image(self, size=16):
+        self.size = size
+        file = self._get_icon_file()
+
+        image = gtk.Image()
+        image.set_from_file(file)
+        return image
+
+    def get_pixbuf(self, grayscale=False, size=16):
+        self.size = size
+        file = self._get_icon_file()
+
+        pixbuf = gtk.gdk.pixbuf_new_from_file_at_size(file, size, size) \
+            if file.endswith('.svg') else gtk.gdk.pixbuf_new_from_file(file)
+        if grayscale:
+            pixbuf = self._set_grayscale(pixbuf)
+
+        return pixbuf
+
+    def _get_icon_file(self):
+        theme = gtk.icon_theme_get_default()
+        info = theme.lookup_icon(self.icon_name, self.size, 0) or \
+            theme.lookup_icon('image-x-generic', self.size, 0)
+        return info.get_filename()
+
+    def _set_grayscale(self, pixbuf):
+        pixbuf_gray = pixbuf.copy()
+        pixbuf.saturate_and_pixelate(pixbuf_gray, 0.0, False)
+        return pixbuf_gray
+
+class LocalIconImage(IconImage):
+
+    def _get_icon_file(self):
+        icon_path = os.path.join(constants.SHARED_DATA_DIR, self.icon_name)
+        return icon_path
+
+class WebIconImage(IconImage):
+
+    def _get_icon_file(self):
+        cache_dir = os.path.join(xdg_cache_home, 'gphotoframe')
+        file = os.path.join(cache_dir, self.icon_name)
+
+        if not os.access(file, os.R_OK):
+            self._download_icon(self.icon_url, cache_dir, self.icon_name)
+
+            super(WebIconImage, self).__init__()
+            file = super(WebIconImage, self)._get_icon_file()
+
+        return file
+
+    def _download_icon(self, icon_url, cache_dir, icon_name):
+        if not os.access(cache_dir, os.W_OK):
+            os.makedirs(cache_dir)
+
+        icon_file = os.path.join(cache_dir, icon_name)
+        urlget = UrlGetWithAutoProxy(icon_url)
+        d = urlget.downloadPage(icon_url, icon_file)
