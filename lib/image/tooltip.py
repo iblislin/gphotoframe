f227cf801870564f8637aaa9f4a9d936bd137b20
yendo0206 <yendo0206@localhost>
2010-05-31 23:04:52 +0900

    Fixed a tooltip format.

--- lib/image/tooltip.py
+++ lib/image/tooltip.py
@@ -51,8 +51,8 @@ class ToolTip(object):
                 tip += "by %s\n" % escape(owner)
             if date:
                 format = self.conf.get_string('date_format') or "%x"
-                tip += "%s\n" %  date if isinstance(date, str) else \
-                    time.strftime(format, time.gmtime(date))
+                tip += "%s\n" % (date if isinstance(date, str) else \
+                    time.strftime(format, time.gmtime(date)))
             if location:
                 tip += "%s\n" % escape(location)
 
