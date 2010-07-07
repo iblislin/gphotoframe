a4d97a7c5affca423a95af386d243472b8062bc5
yendo0206 <yendo0206@localhost>
2010-07-07 17:21:03 +0900

    Removed obsolete has_key methods.

--- lib/utils/EXIF.py
+++ lib/utils/EXIF.py
@@ -1495,7 +1495,7 @@ class EXIF_header:
         
         # Some apps use MakerNote tags but do not use a format for which we
         # have a description, so just do a raw dump for these.
-        #if self.tags.has_key('Image Make'):
+        #if 'Image Make' in self.tags:
         make = self.tags['Image Make'].printable
         #else:
         #    make = ''
