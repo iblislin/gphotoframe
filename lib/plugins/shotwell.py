661b8e548d019be70e97a06e84ddb6c7564e7395
yendo0206 <yendo0206@localhost>
2010-09-26 22:38:15 +0900

    Added the shotwell-16.svg icon.

--- lib/plugins/shotwell.py
+++ lib/plugins/shotwell.py
@@ -12,7 +12,7 @@ import sqlite3
 import gtk
 from gettext import gettext as _
 
-from ..utils.iconimage import IconImage
+from ..utils.iconimage import LocalIconImage
 from base import *
 from fspot.__init__ import *
 from fspot.rating import RateList
@@ -83,10 +83,10 @@ class ShotwellFav(FSpotFav):
         self.sql_table = 'PhotoTable'
         self.db_class = ShotwellDB
 
-class ShotwellIcon(IconImage):
+class ShotwellIcon(LocalIconImage):
 
     def __init__(self):
-        self.icon_name = 'shotwell'
+        self.icon_name = 'shotwell-16.svg'
 
 # sql
 
