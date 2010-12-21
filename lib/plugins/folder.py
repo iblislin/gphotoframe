3e95767c16f5b0791e5050c617c000a70722b713
yendo0206 <yendo0206@localhost>
2010-12-22 01:27:04 +0900

    Fixed an AttributeError (issue #79).

--- lib/plugins/folder.py
+++ lib/plugins/folder.py
@@ -72,7 +72,7 @@ class DirPhotoList(PhotoList):
                  'url'      : 'file://' + fullpath,
                  'filename' : fullpath,
                  'title'    : filename,
-                 'trash'    : Trash(None, fullpath),
+                 'trash'    : Trash(None, fullpath, self.photolist),
                  'icon'     : FolderIcon}
         photo = Photo(data)
         self.photos.append(photo)
