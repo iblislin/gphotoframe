0a3094bcaffb5ae5953e9d6df12abb7f464e3646
yendo0206 <yendo0206@localhost>
2011-04-06 00:23:03 +0900

    Code refactoring.

--- lib/image/actor/share.py
+++ lib/image/actor/share.py
@@ -7,7 +7,7 @@ from ...plugins.tumblr import TumblrShare
 class ActorShareIcon(ActorGeoIcon):
 
     def _check_photo(self):
-        return not self.photo.is_local_file()
+        return self.photo.can_share()
 
     def _get_icon(self):
         return IconImage('emblem-shared')
@@ -18,8 +18,8 @@ class ActorShareIcon(ActorGeoIcon):
     def _on_button_press_cb(self, actor, event):
         print "push!"
 
-        share = TumblrShare(self.photo)
-        share.add()
+        share = TumblrShare()
+        share.add(self.photo)
 
     def _enter_cb(self, w, e, tooltip):
         tooltip.update_text(_("Share on Tumblr"))
