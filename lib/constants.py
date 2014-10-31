de6577d72de8290b41d80a11845ac7702ee75bbf
yendo0206 <yendo0206@localhost>
2014-10-31 23:17:01 +0900

    Fixed a bug that an entry has no title causes a crash.

--- lib/constants.py
+++ lib/constants.py
@@ -5,7 +5,7 @@ from stat import S_IMODE
 
 from xdg.BaseDirectory import *
 
-VERSION = '2.0'
+VERSION = '2.0.1-a1'
 APP_NAME = 'gphotoframe'
 
 SHARED_DATA_DIR = abspath(os.path.join(dirname(__file__), '../share'))
